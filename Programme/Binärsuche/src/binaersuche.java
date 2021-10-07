395ae8365de282f27de50abe6b456e5a809f79bb
Julius Greil <jugreil@tsn.at>
2021-10-07 10:48:29 +0200

    Nach der Besprechung in der Stunde

--- Programme/Binärsuche/src/binaersuche.java
+++ Programme/Binärsuche/src/binaersuche.java
@@ -1,17 +1,21 @@
 import java.util.Date;
+import java.util.Scanner;
 
 class binaersuche {
 
+	static int counter = 0;
+	static long ende;
 	public static void main(String[] args) {
-		long anzahl = 200000000;
-		long[] zahlen = new long[(int) anzahl];
-		for (long i = 0; i < anzahl; i++) {
-			zahlen[(int)i] = i;
-		}
-		long gesucht = 200000001;
-
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Wie viele zahlen sollen in Ihr Array");
+		int anzahl = Integer.parseInt(scanner.next());
+		System.out.println("Welche zahl wollen sie nun finden");
+		int gesucht = Integer.parseInt(scanner.next());
+		int[] zahlen = arrayFiller(anzahl);
 		boolean gefunden = binaereSuche(zahlen, gesucht);
+		System.out.println(ende + "|" + counter);
 		gefunden = sucheIterativ(gesucht, zahlen);
+		System.out.println(ende + "|" + counter);
 		if (gefunden) {
 			System.out.println("Die Zahl ist enthalten");
 		} else {
@@ -19,18 +23,16 @@ class binaersuche {
 		}
 	}
 
-	public static boolean binaereSuche(long[] myList, long zahl) {
+	public static boolean binaereSuche(int[] myList, int zahl) {
 		long start = System.currentTimeMillis();
-		long low = 0;
-		long high = myList.length - 1;
-		long i = 0;
+		int low = 0;
+		int high = myList.length - 1;
 		while (low <= high) {
-			i++;
-			long index = (low + high) / 2;
+			counter++;
+			int index = (low + high) / 2;
 
 			if (zahl == myList[(int) index]) {
-				long ende= System.currentTimeMillis()-start;
-				System.out.println(ende + "|" + i);
+				ende = System.currentTimeMillis() - start;
 				return true;
 			} else if (zahl < myList[(int) index]) {
 				high = index - 1; // alle Zahlen höher wie der such index werden entfernt
@@ -38,41 +40,53 @@ class binaersuche {
 				low = index + 1; // alle Zahlen unter dem such index werden entfernt
 			}
 		}
-		long ende= System.currentTimeMillis()-start;
-		System.out.println(ende + "|" + i);
+		 ende = System.currentTimeMillis() - start;
+		
 		return false;
 	}
-	
-	public static boolean sucheIterativ(long zahl, long[] mList) {
-		
+
+	public static boolean sucheIterativ(int zahl, int[] mList) {
+
 		long start = System.currentTimeMillis();
-        for(long i = 0; i < mList.length; i++) {
-             
-            if(mList[(int)i] == zahl) {
-            	long ende = System.currentTimeMillis()-start;
-				System.out.println(ende + "|" + i);
-                return true;
-            }
-        }
-        long ende= System.currentTimeMillis()-start;
-		System.out.println(ende + "|" + mList.length);
-        return false;
-    }
-	   boolean binarySearch(int arr[], int low, int high, int search)
-	    {
-	        if (high >= low) {
-	            int index = low + (high - low) / 2;
-	            if (arr[index] == search) {
-	                return true;
-	            }
-	            if (arr[index] > search) {
-	                return binarySearch(arr, low, index - 1, search);
-	            }
-	            return binarySearch(arr, index + 1, high, search);
-	        }
-	  
-	        
-	        return false;
-	    }
+		for (int i = 0; i < mList.length; i++) {
+			counter++;
+			if (mList[(int) i] == zahl) {
+				 ende = System.currentTimeMillis() - start;
+				
+				return true;
+			}
+		}
+		 ende = System.currentTimeMillis() - start;
+		return false;
+	}
+
+	boolean binarySearch(int arr[], int low, int high, int search) {
+		if (high >= low) {
+			int index = (low + high) / 2;
+			if (arr[index] == search) {
+				return true;
+			}
+			if (arr[index] > search) {
+				return binarySearch(arr, low, index - 1, search);
+			}
+			return binarySearch(arr, index + 1, high, search);
+		}
+
+		return false;
+	}
+
+	public static int[] arrayFiller(int anzahl) {
+		int[] zahlen = new int[(int) anzahl];
+		for (int i = 0; i < zahlen.length; i++) {
+			zahlen[(int) i] = i + 1;
+		}
+		return zahlen;
+	}
+
+	public void arrayPrinter(int[] ar) {
+		for (int i = 0; i < ar.length; i++) {
+			System.out.println(ar[i]);
+		}
+	}
 
 }
