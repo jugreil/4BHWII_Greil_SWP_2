cc759795ac30b3ea6053156b27fdda79ffd4d6b6
Julius Greil <jugreil@tsn.at>
2021-10-07 07:41:30 +0200

    Fertige Programme

--- Programme/Binärsuche/src/binaersuche.java
+++ Programme/Binärsuche/src/binaersuche.java
@@ -1,32 +1,62 @@
-public class binaersuche {
+import java.util.Date;
 
+class binaersuche {
 
-    private static int counter; 
+	public static void main(String[] args) {
+		long anzahl = 200000000;
+		long[] zahlen = new long[(int) anzahl];
+		for (long i = 0; i < anzahl; i++) {
+			zahlen[(int)i] = i;
+		}
+		long gesucht = 200000001;
 
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
-        int[] zahlen = {1,2,4,66,2,4,66};
+		boolean gefunden = binaereSuche(zahlen, gesucht);
+		gefunden = sucheIterativ(gesucht, zahlen);
+		if (gefunden) {
+			System.out.println("Die Zahl ist enthalten");
+		} else {
+			System.out.println("So So da hasche wohl a zahl genommen de nit drein is");
+		}
+	}
 
-        System.out.println(counter);
+	public static boolean binaereSuche(long[] myList, long zahl) {
+		long start = System.currentTimeMillis();
+		long low = 0;
+		long high = myList.length - 1;
+		long i = 0;
+		while (low <= high) {
+			i++;
+			long index = (low + high) / 2;
 
-        if(sucheIterativ(2, zahlen)) {
-            System.out.println("Hype gefunden");
-        } else {
-            System.out.println("Doch ned gfunden");
-        }
-        System.out.println("Schritte: " + counter);
-    }
-
-
-    public static boolean sucheIterativ(int zahl, int[] mList) {
-
-        for(int i = 0; i < mList.length; i++) {
-            counter = i;
-            if(mList[i] == zahl) {
+			if (zahl == myList[(int) index]) {
+				long ende= System.currentTimeMillis()-start;
+				System.out.println(ende + "|" + i);
+				return true;
+			} else if (zahl < myList[(int) index]) {
+				high = index - 1; // alle Zahlen höher wie der such index werden entfernt
+			} else {
+				low = index + 1; // alle Zahlen unter dem such index werden entfernt
+			}
+		}
+		long ende= System.currentTimeMillis()-start;
+		System.out.println(ende + "|" + i);
+		return false;
+	}
+	
+	public static boolean sucheIterativ(long zahl, long[] mList) {
+		
+		long start = System.currentTimeMillis();
+        for(long i = 0; i < mList.length; i++) {
+             
+            if(mList[(int)i] == zahl) {
+            	long ende = System.currentTimeMillis()-start;
+				System.out.println(ende + "|" + i);
                 return true;
             }
         }
-
+        long ende= System.currentTimeMillis()-start;
+		System.out.println(ende + "|" + mList.length);
         return false;
     }
+
 }
