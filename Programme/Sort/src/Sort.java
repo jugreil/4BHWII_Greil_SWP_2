0a25d923097c7313c851d5b313daba6ee69cc580
Julius Greil <jugreil@tsn.at>
2021-12-09 08:14:37 +0100

    Bubble sort als HÜ fertig

--- Programme/Sort/src/Sort.java
+++ Programme/Sort/src/Sort.java
@@ -1,24 +1,5 @@
 import java.util.Random;
 
-/*
- * import random 
- 
-arr = []
-
-
-for x in range(100):
-    arr.append(random.randrange(100))
-
-for x in range(len(arr)-1):
-    for y in range(0, (len(arr)-1)-x):
-        if arr[y]>arr[y+1]:
-            arr[y],arr[y+1] = arr[y+1],arr[y]
-
-
-
-print(*arr)
-
- */
 public class Sort {
 	
 	
